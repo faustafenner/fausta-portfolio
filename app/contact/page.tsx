9e4a6731408524f8229a13d3dc53ad9627455696
faustafenner <fennerfausta@gmail.com>
2026-01-19 01:26:04 -0500

    added logo

---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <section className="my-8 flex justify-end">
+      <p className="text-2xl mb-8 tracking-tighter text-right border-b border-black pb-2 inline-block">
+        contact
+      </p>
+    </section>
+  );
+}
