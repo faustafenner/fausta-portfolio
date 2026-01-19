b0c8fb15ef7fe29562916a90528ad67a273b3776
faustafenner <fennerfausta@gmail.com>
2026-01-19 00:29:37 -0500

    base edits

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,37 @@
-import { BlogPosts } from 'app/components/posts'
+import Link from "next/link";
+
+function ArrowIcon() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="ml-1"
+    >
+      <path
+        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
 
 export default function Page() {
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
-      </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
-      </p>
-      <div className="my-8">
-        <BlogPosts />
+    <section className="flex justify-center items-center my-64">
+      <div className="flex gap-20 text-4xl">
+        <Link href="/design" className="flex items-center">
+          design
+          <ArrowIcon />
+        </Link>
+
+        <Link href="/engineering" className="flex items-center">
+          engineering
+          <ArrowIcon />
+        </Link>
       </div>
     </section>
-  )
+  );
 }
